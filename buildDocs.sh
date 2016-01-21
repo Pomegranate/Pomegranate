a5159cafcc12f4b94d15aef2be869218756263d9
Jim Bulkowski <jim.b@paperelectron.com>
2016-01-21 02:02:02 -0500

    Wiping out legacy code in 3.0_beta

--- buildDocs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./node_modules/.bin/jsdoc2md -t docs/configuration.hbs lib/Configuration/Config.js > _gh-pages/docs/configuration.md
-./node_modules/.bin/jsdoc2md -t docs/logging.hbs lib/Utilities/LogEmitter.js > _gh-pages/docs/logging.md
-./node_modules/.bin/jsdoc2md -t docs/pomegranate.hbs lib/Pomegranate.js > _gh-pages/docs/pomegranate.md
